7505a8aac381027c7a82154d3789efbc615dd534
quynhanhsang <sangtrank64@gmail.com>
2022-08-31 10:35:12 +0700

    first commit

---
+++ Bai2/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Bai2 {
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion();
+            lion.SetMe("Lion", 200);
+            lion.Show();
+
+            Console.WriteLine();
+
+            Tiger tiger = new Tiger();
+            tiger.SetMe("Tiger", 100);
+            tiger.Show();
+        }
+
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+
+            public abstract int Weight { get; set; }
+
+            public abstract void SetMe(string name, int weight);
+
+            public abstract void Show();
+
+            protected Animal()
+            {
+            }
+
+            protected Animal(string name, int weight)
+            {
+                Name = name;
+                Weight = weight;
+            }
+        }
+
+        class Lion : Animal
+        {
+            public override string Name { get; set; }
+            public override int Weight { get; set; }
+
+            public Lion() { }
+
+            public override void SetMe(string name, int weight)
+            {
+                Name = name;
+                Weight = weight;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine("Name: " + Name + ", Weight: " + Weight);
+            }
+        }
+
+        class Tiger : Animal
+        {
+            public override string Name { get; set; }
+            public override int Weight { get; set; }
+
+            public Tiger()
+            {
+            }
+
+            public override void SetMe(string name, int weight)
+            {
+                Name = name;
+                Weight = weight;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine("Name: " + Name + ", Weight: " + Weight);
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            double Radius = 38.64;
+            double Height = 22.48;
+            double BaseArea = Radius * Radius * Math.PI;
+            double LateralArea = 2 * Math.PI * Radius * Height;
+            double TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            double Volume = Math.PI * Radius * Radius * Height;
+            Console.WriteLine("Radius: 38.64 , Height: 22.48");
+
+            Console.WriteLine("Base :" + BaseArea );
+            Console.WriteLine("Lateral :" + LateralArea);
+            Console.WriteLine("Total:" + TotalArea);
+            Console.WriteLine("Volume :" + Volume);
+            Console.ReadKey();
+
+
+        }
+    }
+}
